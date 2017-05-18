1a1abe66ae22a651e400dcb36a194efbd547b63d
Raju Rimal <therimalaya@gmail.com>
2017-05-18 18:41:13 +0200

    initial commit

--- _deploy.sh
+++ _deploy.sh
@@ -5,11 +5,11 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "therimalaya@gmail.com"
+git config --global user.name "Raju Rimal"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
+#git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+cd ggplot2-book
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
